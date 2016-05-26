1bba0645717c31621957a803e7592d77d6cd6eb7
Sean Kirkpatrick <seanmkirkpatrick@gmail.com>
2016-05-26 15:39:44 -0400

    Initial Commit

---
+++ StreamAndSave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StreamAndSave
+//
+//  Created by Sean M Kirkpatrick on 5/26/16.
+//  Copyright © 2016 Proto Venture Technology, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
